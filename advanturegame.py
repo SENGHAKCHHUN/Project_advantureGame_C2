8c4393aef3b755a3e367b6f980b4b886c290d8c3
SENGHAK.CHHUN <SENGHAK.CHHUN@pnc.lan>
2023-10-21 20:09:09 +0700

    #new file

--- advanturegame.py
+++ advanturegame.py
@@ -234,6 +234,8 @@ canvas.tag_bind("level3","<Button-1>",level3)
 #========================= REMOTES =================
 window.bind("<Key>", start_move)
 window.bind("<KeyRelease>", stop_move)
+window.bind("<KeyRelease>", stop_move)
+
 home()
 
 #========================= DISPLAY WINDOW =================
